9a8a63207b207ee2c99efa1522a45b2e3bf63a85
Matias Maravi <matias.maravi@utec.edu.pe>
2022-06-09 00:14:20 -0500

    Creada la funcion suma_dos_senos

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("hola mundo")
+from math import atan2, cos, sin, pow, sqrt, pi
+def suma_dos_senos(amplitudA,amplitudB,faseA,faseB):
+    C = sqrt( pow((amplitudA*cos(faseA) + amplitudB*sin(faseB)),2) + pow((amplitudA*sin(faseA) + amplitudB*cos(faseB)),2)  )
+    Fase = (pi/2) - atan2((amplitudA*cos(faseA) + amplitudB*sin(faseB))/(amplitudA*sin(faseA) + amplitudB*cos(faseB)))
+    return (C, Fase)
