c5acf1f09182cb00d743ba6ea5898dd2c3aa93cc
Matias Maravi <matias.maravi@utec.edu.pe>
2022-06-21 01:03:51 -0500

    FUNCION PATRON CREADA

--- main.py
+++ main.py
@@ -5,20 +5,26 @@ def suma_dos_senos(amplitudA,amplitudB,faseA,faseB):
     Fase = (pi/2) - atan2((amplitudA*cos(faseA) + amplitudB*sin(faseB)),(amplitudA*sin(faseA) + amplitudB*cos(faseB)))
     return (amplitud_r, Fase)
 
-
+#E0 = 1 para simplificar el problema
 
 def patron_de_intensidad(Lambda,distancia,angulo,numero,lista):
     amplitudes = []
-    Yp = distancia*tan(angulo)
-    k = (2*pi)/Lambda
-    for i in range(len(lista)-1):
-        r1 = sqrt(pow(distancia,2)+pow((Yp-lista[i]),2))
+    razon = (2*angulo)/(numero-1)
+    for j in range(0,numero):
+        Yp = distancia*tan(angulo)
+        k = (2*pi)/Lambda
+        r1 = sqrt(pow(distancia,2)+pow((Yp-lista[0]),2))
         fase1 = k*r1
-        r2 = sqrt(pow(distancia,2)+pow((Yp-lista[i+1]),2))
-        fase2 = k*r2
-        (A,f) = suma_dos_senos(lista[i],lista[i+1],fase1,fase2)
-        amplitudes.append(pow(A,2))
-    
-
+        AMPLITUD_TOTAL = 1/r1
+        FASE_TOTAL = fase1
+        for i in range(1,len(lista)):
+            r2 = sqrt(pow(distancia,2)+pow((Yp-lista[i]),2))
+            fase2 = k*r2
+            amplitud2 = 1/r2
+            (A,f) = suma_dos_senos(AMPLITUD_TOTAL,amplitud2,FASE_TOTAL,fase2)
+            AMPLITUD_TOTAL = A
+            FASE_TOTAL = f
+        amplitudes.append(pow(AMPLITUD_TOTAL,2))
+        angulo-=razon
     return amplitudes
-
+    
